5a0f3f21fcfb27c47dd6ef4a27d7b11f1ad778a4
Andy Hoffman <antibland@gmail.com>
2018-03-18 17:30:46 -0700

    Bring back service worker

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import './css/Icons.css';
 import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
-import { unregister } from './registerServiceWorker';
-unregister();
+import registerServiceWorker from './registerServiceWorker';
+// import { unregister } from './registerServiceWorker';
+// unregister();
 
 ReactDOM.render((
   <BrowserRouter>
@@ -14,4 +14,4 @@ ReactDOM.render((
   </BrowserRouter>
 ), document.getElementById('root'));
 
-//registerServiceWorker();
+registerServiceWorker();
