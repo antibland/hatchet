38583750be6966378606ddabf2b2d5df9b4669d5
Andy Hoffman <antibland@gmail.com>
2018-03-20 13:54:23 -0700

    Overhaul newFight endpoint logic

--- controllers/fight.js
+++ controllers/fight.js
@@ -55,59 +55,67 @@ exports.getUserFights = async (req, res) => {
 
 exports.newFight = async (req, res) => {
   let user = await User.findById(req.params.userId);
+  let opponent = null;
   let target = req.body.target;
-  let userOptions = {
-    email: user.email,
-    username: user.username
-  };
+
+  // default fight options
   let fightOptions = {
     type: req.body.type,
     title: req.body.title,
     antagonist: user,
-    text: {
-      for: req.body.beef
-    }
+    text: { for: req.body.beef }
   };
 
-  const fightSomeone = async () => {
+  const create = async () => {
+    let fight = await new Fight(fightOptions);
+
+    await user.fights.push(fight);
+
+    await fight.save(err => {
+      if (err) throw err;
+    });
+
+    await user.save(err => {
+      if (err) throw err;
+      res.redirect('back');
+    });
+  };
+
+  if (target === 'world') {
+    fightOptions.isLive = true;
+    create();
+  } else if (target === 'someone') {
     let oppenentRef = req.body.opponent;
     let findBy = validator.validate(oppenentRef)
                   ? { email: oppenentRef }
                   : { username: oppenentRef };
 
-    await User.findOne(findBy)
-      .exec((err, user) => {
-        if (err) {
-          return res.status(401).json({
-            type: 'failure',
-            message: 'User lookup error'
-          });
-        } else if (!user) {
-          // Could not find the user
-          console.log('User not found—email info sent.')
-        } else {
-          // User found
-          console.log(`We've let ${user.username} know you've got an axe to grind.`);
-          fightOptions.defender = user;
-        }
-      });
-  };
+    opponent = await User.findOne(findBy);
+    //console.log('opponent', opponent);
 
-  if (target === 'someone') {
-    await fightSomeone();
-  } else {
-    fightOptions.isLive = true;
+    if (!opponent || opponent === null) {
+      // Case: User not found via email lookup. Send an invite
+      if (validator.validate(oppenentRef)) {
+        console.log(`We just sent a site invitation to ${oppenentRef}...`);
+        res.status(400).json({
+          type: 'failure',
+          message: `We just sent a site invitation to ${oppenentRef}...`
+        });
+        res.redirect('back');
+      } else {
+        // Case: User not found via username lookup. Give up
+        console.log(`Sorry, we have no record of ${oppenentRef}`);
+        res.status(400).json({
+          type: 'failure',
+          message: `Sorry, we have no record of ${oppenentRef}`
+        });
+        res.redirect('back');
+      }
+    } else {
+      // Opponent exists, so continue
+      fightOptions.defender = opponent;
+      create();
+    }
   }
 
-  let fight = await new Fight(fightOptions);
-
-  await fight.save(err => {
-    if (err) throw err;
-  });
-
-  await user.fights.push(fight);
-  await user.save(err => {
-    if (err) throw err;
-    res.redirect('back');
-  });
 }
