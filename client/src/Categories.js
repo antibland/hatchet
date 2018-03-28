90887200eec79b8ce839a92f788fcc9ddd5a1c9c
Andy Hoffman <antibland@gmail.com>
2018-03-28 00:50:01 -0700

    First pass category images

--- client/src/Categories.js
+++ client/src/Categories.js
@@ -5,6 +5,63 @@ import './css/Categories.css';
 import ButtonLink from './shared/components/ButtonLink';
 import { auth } from './Auth.js';
 
+function getImg(word) {
+  let prefix = '/categories/category-';
+  let img = '';
+  let w = word.replace('\'', '').toLowerCase();
+
+  switch (w) {
+    case 'lovers':
+      img = <img
+              className='categoriesImg'
+              src={`${prefix}lover.png`}
+              srcSet={`${prefix}lover.png 1x ${prefix}lover@2x.png 2x`}
+              alt="Lover's Quarrel"
+            />
+      break;
+    case 'workplace':
+      img = <img
+               className='categoriesImg'
+              src={`${prefix}coworker.png`}
+              srcSet={`${prefix}coworker.png 1x ${prefix}coworker@2x.png 2x`}
+              alt="Workplace Squabble"
+            />
+      break;
+    case 'friend':
+      img = <img
+              className='categoriesImg'
+              src={`${prefix}friend.png`}
+              srcSet={`${prefix}friend.png 1x ${prefix}friend@2x.png 2x`}
+              alt="Friend Fight"
+            />
+      break;
+    case 'roommate':
+      img = <img
+              className='categoriesImg'
+              src={`${prefix}roommate.png`}
+              srcSet={`${prefix}roommate.png 1x ${prefix}roommate@2x.png 2x`}
+              alt="Roommate Rumble"
+            />
+      break;
+    case 'family':
+      img = <img
+              className='categoriesImg'
+              src={`${prefix}family.png`}
+              srcSet={`${prefix}family.png 1x ${prefix}family@2x.png 2x`}
+              alt="Family Feud"
+            />
+      break;
+    case 'world':
+      img = <img
+              className='categoriesImg'
+              src={`${prefix}everyone.png`}
+              alt="World War"
+            />
+      break;
+  }
+  return img;
+}
+
 const Categories = () => (
   <div>
     <ul className='categories threeByThree'>
@@ -12,11 +69,11 @@ const Categories = () => (
         commonData.categories.map((item, index) => {
           let [ firstWord, lastWord ] = item.split(' ');
           let url = '/categories/' + item.replace('\'', '').toLowerCase().split(' ').join('_');
-
+          let categoryImg = getImg(firstWord);
           return (
             <li key={index}>
               <Link to={url}>
-                <img alt='placeholder' src='https://via.placeholder.com/150x150' />
+                { categoryImg }
                 <span className='firstWord'>{firstWord}</span>
                 <span>{lastWord}</span>
               </Link>
