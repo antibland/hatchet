5359dbd68bf94c4a2932b2038de97b666b783d8e
Andy Hoffman <antibland@gmail.com>
2018-04-01 12:37:35 -0700

    Better mobile styles for bottom navigation

--- client/src/BottomMenu.js
+++ client/src/BottomMenu.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Avatar from './shared/components/Avatar';
 import { auth } from './Auth.js';
 import './css/BottomMenu.css';
 
@@ -77,7 +76,7 @@ const links = {
 }
 
 const BottomMenu = () => (
-  <nav role="secondary" className="bottomMenuContainer">
+  <nav className="bottomMenuContainer">
     <ul className="bottomMenu">
       <li>
         <NavLink className="nav-link" exact to={links.profile}>
@@ -102,28 +101,6 @@ const BottomMenu = () => (
           <IconWatching />
         </NavLink>
       </li>
-
-      {/* <li><NavLink className="nav-link" exact to='/create'>Start a Hatchet</NavLink></li>
-      <li><NavLink className="nav-link avatar" exact to='/profile'>
-        { auth.user.avatar !== null &&
-          auth.user.avatar !== '' &&
-          typeof auth.user.avatar !== 'undefined'
-          ? <Avatar
-              imgpath={auth.user.avatar}
-              width='40px'
-              height='40px'
-              styles={{ marginRight: '.4em' }} />
-          : <svg aria-hidden="true" className="user-icon">
-              <use xlinkHref="./symbols/svg-defs.svg#user-icon" />
-            </svg>
-        }
-
-        <span className="username">
-          {auth.user.username}
-        </span>
-      </NavLink>
-      </li>
-      <li><NavLink className="nav-link" exact to='/logout'>Logout</NavLink></li> */}
     </ul>
   </nav>
 );
