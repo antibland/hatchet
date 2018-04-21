a08c63c97aced1e04d14bd5e6dadc0e20723091b
Andy Hoffman <antibland@gmail.com>
2018-04-21 01:49:30 -0700

    Add step 2 logic and styles

--- client/src/Categories.js
+++ client/src/Categories.js
@@ -1,30 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import commonData from './shared/commonData';
 import { Link } from 'react-router-dom';
 import './css/Categories.css';
 import utilities from './shared/utilities';
 
-const Categories = () => (
-  <div>
-    <ul className='categories threeByThree'>
-      {
-        commonData.categories.map((item, index) => {
-          let [ firstWord, lastWord ] = item.split(' ');
-          let url = '/categories/' + item.replace('\'', '').toLowerCase().split(' ').join('_');
-          let categoryImg = utilities.getCategoryImage(firstWord);
-          return (
-            <li key={index}>
-              <Link to={url}>
-                { categoryImg }
-                <span className='firstWord'>{firstWord}</span>
-                <span>{lastWord}</span>
-              </Link>
-            </li>
-          )
-        })
+class Categories extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeButton: null,
+      firstButtonClick: true
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState({
+      firstButtonClick: false,
+      activeButton: e.currentTarget.dataset.id
+    })
+  }
+
+  render() {
+    let { activeButton, firstButtonClick } = this.state;
+
+    return (
+      <div>
+        <ul className='categories threeByThree'>
+        {
+          commonData.categories.map((item, index) => {
+            let [ firstWord, lastWord ] = item.split(' ');
+            let url = '/categories/' + item.replace('\'', '').toLowerCase().split(' ').join('_');
+            let categoryImg = utilities.getCategoryImage(firstWord);
+            return (
+              <li key={index}>
+                { this.props.mode === 'button'
+                    ? <button
+                        data-id={`button-${index}`}
+                        onClick={(e) => {
+                          this.props.onClick();
+                          this.handleClick(e);
+                        }}
+                        className={
+                          activeButton === `button-${index}`
+                            ? 'active'
+                            : firstButtonClick === false
+                              ? 'inactive'
+                              : ''
+                        }>
+                        { categoryImg }
+                        <span className='firstWord'>{firstWord}</span>
+                        <span>{lastWord}</span>
+                      </button>
+                    : <Link to={url}>
+                        { categoryImg }
+                        <span className='firstWord'>{firstWord}</span>
+                        <span>{lastWord}</span>
+                      </Link>
+                }
+              </li>
+            )
+          })
       }
-    </ul>
-  </div>
-);
+      </ul>
+    </div>
+    )
+  }
+}
+
+Categories.defaultProps = {
+  mode: 'link'
+};
 
 export default Categories;
--- client/src/Create/Step2.js
+++ client/src/Create/Step2.js
@@ -2,15 +2,36 @@ import React, { Component } from 'react';
 import Categories from '../Categories';
 
 class Step2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isValid: false
+    };
+    this.setValidTrue = this.setValidTrue.bind(this);
+  }
+
+  setValidTrue() {
+    this.setState({ isValid: true })
+  }
+
   render() {
     if (this.props.currentStep !== 2) {
       return null;
     }
 
+    const { isValid } = this.state;
+
     return (
       <div className="stepContainer">
         <h2>Pick a Category</h2>
-        <Categories />
+        <Categories mode='button' onClick={this.setValidTrue} />
+        <button
+          type="submit"
+          onClick={this.props.afterValid}
+          style={{ display: 'block', margin: '2em auto'}}
+          disabled={!isValid}
+          className="button">Continue
+        </button>
       </div>
     )
   }
