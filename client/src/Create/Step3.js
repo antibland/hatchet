63491febc04b0d6f8a745dc731fe6909dc6dfba3
Andy Hoffman <antibland@gmail.com>
2018-04-24 21:21:36 -0700

    Goodbye rte, hello regular textarea

--- client/src/Create/Step3.js
+++ client/src/Create/Step3.js
@@ -1,70 +1,29 @@
 import React, {Component} from 'react';
-import RichTextEditor from 'react-rte';
-import PropTypes from 'prop-types';
-
 class Step3 extends Component {
   constructor() {
     super();
     this.state = {
-      value: RichTextEditor.createEmptyValue(),
-      countRemaining: 1011,
-      isValid: false
+      count: 0,
+      isValid: true
     };
 
-    this.onChange = this.onChange.bind(this);
+    this.handleTextareaChange = this.handleTextareaChange.bind(this);
   }
-  static propTypes = {
-    onChange: PropTypes.func
-  };
 
-  onChange(value) {
-    this.setState({
-      value,
-      countRemaining: Number(1000 - value.toString('html').length)
-    });
+  handleTextareaChange(e) {
+    let count = e.target.value.length;
+    this.setState({ count });
 
-    const { countRemaining } = this.state;
-
-    if (countRemaining > 0 && countRemaining <= 1000) {
-      this.setState({ isValid: true })
+    if (count >= 0 && count <= 1000) {
+      this.setState({ isValid: true });
     } else {
-      this.setState({ isValid: false })
-    }
-
-    if (this.props.onChange) {
-      console.log('changed')
-      // Send the changes up to the parent component as an HTML string.
-      // This is here to demonstrate using `.toString()` but in a real app it
-      // would be better to avoid generating a string on each change.
-      console.log('value', value)
-      this.props.onChange(
-        value.toString('html')
-      );
+      this.setState({ isValid: false });
     }
   }
 
   render() {
-    const toolbarConfig = {
-      // Optionally specify the groups to display (displayed in the order listed).
-      display: ['INLINE_STYLE_BUTTONS', 'BLOCK_TYPE_BUTTONS', 'HISTORY_BUTTONS'],
-      INLINE_STYLE_BUTTONS: [
-        {label: 'Bold', style: 'BOLD'},
-        {label: 'Italic', style: 'ITALIC'},
-        {label: 'Underline', style: 'UNDERLINE'}
-      ],
-      BLOCK_TYPE_DROPDOWN: [
-        {label: 'Normal', style: 'unstyled'},
-        {label: 'Heading Large', style: 'header-one'},
-        {label: 'Heading Medium', style: 'header-two'},
-        {label: 'Heading Small', style: 'header-three'}
-      ],
-      BLOCK_TYPE_BUTTONS: [
-        {label: 'UL', style: 'unordered-list-item'},
-        {label: 'OL', style: 'ordered-list-item'}
-      ]
-    };
-
-    const { countRemaining, isValid } = this.state;
+    const { count, isValid } = this.state;
+    const countRemaining = 1000 - count;
     const error = isValid === false ? 'error' : '';
     const classes = `charsRemaining ${error}`;
 
@@ -72,13 +31,27 @@ class Step3 extends Component {
       <div className="stepContainer">
         <div className="inner">
           <h2>What happened?</h2>
-          <RichTextEditor
-            toolbarConfig={toolbarConfig}
-            value={this.state.value}
-            onChange={this.onChange}
-            placeholder="Your argument goes here"
-          />
-          <div className={classes}>{ countRemaining }</div>
+
+          <div className="beefWrap">
+            <textarea
+              required
+              onInput={this.handleTextareaChange}
+              onBlur={this.handleTextareaChange}
+              name="beef"
+              id="beef"
+              placeholder="Your argument goes here">
+            </textarea>
+            <div className={classes}>{ countRemaining }</div>
+          </div>
+
+          <button
+            type="submit"
+            onClick={this.props.afterValid}
+            style={{ display: 'block', margin: '2em auto 0'}}
+            disabled={count <= 0 || !isValid}
+            className="button">Complete
+          </button>
+
         </div>
       </div>
     )
