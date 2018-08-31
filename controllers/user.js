352ebfdb213a6b8918ceae2082ef772471f21d83
Andy Hoffman <antibland@gmail.com>
2018-08-31 16:19:49 -0700

    Prevent logout failure if user is null

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,16 @@
-const User = require('../models/user');
-const Token = require('../models/verification');
-const jwt = require('jsonwebtoken');
-const nodemailer = require('nodemailer');
-const crypto = require('crypto');
-const env = process.env.NODE_ENV || 'development';
+const User = require("../models/user");
+const Token = require("../models/verification");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
+const env = process.env.NODE_ENV || "development";
 const validator = require("email-validator");
 
 // Helper functions
 
 const getAvatarPath = path => {
   if (path) {
-    return '/avatars/' + path.substr(path.lastIndexOf('/')+1);
+    return "/avatars/" + path.substr(path.lastIndexOf("/") + 1);
   }
 };
 
@@ -21,7 +21,7 @@ const sendEmail = opts => {
   const user = opts.user;
 
   let transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
+    host: "smtp.ethereal.email",
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
@@ -31,32 +31,37 @@ const sendEmail = opts => {
   });
 
   let mailOptions = {
-    from: 'no-reply@yourwebapplication.com',
+    from: "no-reply@yourwebapplication.com",
     to: user.email,
-    subject: 'Account Verification Token',
-    text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/api\/confirmation\/' + token.token + '.\n'
+    subject: "Account Verification Token",
+    text:
+      "Hello,\n\n" +
+      "Please verify your account by clicking the link: \nhttp://" +
+      req.headers.host +
+      "/api/confirmation/" +
+      token.token +
+      ".\n"
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
-    console.log('Message sent: %s', info.messageId);
+    console.log("Message sent: %s", info.messageId);
     // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
     res.json({
-      type: 'success',
-      message: 'We\'ve just emailed you a verification link. Make things real!'
+      type: "success",
+      message: "We've just emailed you a verification link. Make things real!"
     });
   });
-}
+};
 
 exports.join = async (req, res) => {
   if (req.body.username && req.body.email && req.body.password) {
-
     const regex = /^[a-zA-Z0-9_.-]*$/g;
     if (!regex.test(req.body.username)) {
       return res.status(401).json({
-        type: 'failure',
-        message: 'Sorry, but this username contains invalid characters.'
+        type: "failure",
+        message: "Sorry, but this username contains invalid characters."
       });
     }
 
@@ -70,31 +75,34 @@ exports.join = async (req, res) => {
 
     try {
       user = await User.create(userData);
-    } catch(err) {
+    } catch (err) {
       return res.status(401).json({
-        type: 'failure',
-        message: 'The username or email address is not unique.'
+        type: "failure",
+        message: "The username or email address is not unique."
       });
     }
 
     // Create a verification token for this user
-    await Token.create({
-      _userId: user._id,
-      token: crypto.randomBytes(16).toString('hex')}, (err, token) => {
-      // Save the verification token
-      token.save(saveErr => {
-        if (saveErr) {
-          return res.status(500).json({
-            type: 'failure',
-            message: 'Something went wrong on our end. Please try that again.'
-          });
-        }
+    await Token.create(
+      {
+        _userId: user._id,
+        token: crypto.randomBytes(16).toString("hex")
+      },
+      (err, token) => {
+        // Save the verification token
+        token.save(saveErr => {
+          if (saveErr) {
+            return res.status(500).json({
+              type: "failure",
+              message: "Something went wrong on our end. Please try that again."
+            });
+          }
 
-        // Send verification email
-        sendEmail({ req, res, token, user });
-      });
-    });
-
+          // Send verification email
+          sendEmail({ req, res, token, user });
+        });
+      }
+    );
   }
 };
 
@@ -102,28 +110,32 @@ exports.login = (req, res) => {
   User.authenticate(req.body.email, req.body.password, async (error, user) => {
     if (error || !user) {
       return res.status(401).json({
-        type: 'failure',
-        message: 'Your email or password is incorrect. It happens.'
+        type: "failure",
+        message: "Your email or password is incorrect. It happens."
       });
     }
 
     if (!user.isVerified) {
       return res.status(401).json({
-        type: 'failure',
-        message: 'Almost there. Please check your email and verify your account.'
+        type: "failure",
+        message:
+          "Almost there. Please check your email and verify your account."
       });
     }
 
-    const token = jwt.sign({
-      email: user.email,
-      _id: user._id
-    }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      {
+        email: user.email,
+        _id: user._id
+      },
+      process.env.JWT_SECRET
+    );
 
     user.sessionToken = token;
     await user.save(err => {
       if (err) {
         return res.status(500).send({
-          type: 'failure',
+          type: "failure",
           message: err.message
         });
       }
@@ -132,7 +144,7 @@ exports.login = (req, res) => {
     let avatar = user.avatar.path;
 
     return res.status(200).json({
-      type: 'success',
+      type: "success",
       token,
       user: {
         userid: user._id,
@@ -140,99 +152,119 @@ exports.login = (req, res) => {
         avatar
       }
     });
-
   });
 };
 
 exports.resetPassword = (req, res) => {
-  User.findOne({ email: req.body.email })
-    .exec((err, user) => {
-      if (err) {
-        return res.status(401).json({
-          type: 'failure',
-          message: 'A system error occurred. We are looking into it.'
-        });
-      } else if (!user) {
-        return res.status(401).json({
-          type: 'failure',
-          message: `There is no account associated with ${req.body.email}.`
-        });
-      } else {
-        // Generate password reset email and send email it
-        return res.status(200).json({
-          type: 'success',
-          message: `A link to reset your password has been sent to ${req.body.email}.`
-        })
-      }
-    });
-}
+  User.findOne({ email: req.body.email }).exec((err, user) => {
+    if (err) {
+      return res.status(401).json({
+        type: "failure",
+        message: "A system error occurred. We are looking into it."
+      });
+    } else if (!user) {
+      return res.status(401).json({
+        type: "failure",
+        message: `There is no account associated with ${req.body.email}.`
+      });
+    } else {
+      // Generate password reset email and send email it
+      return res.status(200).json({
+        type: "success",
+        message: `A link to reset your password has been sent to ${
+          req.body.email
+        }.`
+      });
+    }
+  });
+};
 
 exports.logout = async (req, res) => {
   let { userId } = req.params;
   let user = await User.findById(userId);
-  user.sessionToken = undefined;
 
-  await user.save(err => {
-    if (err) {
-      return res.status(500).json({
-        type: 'failure',
-        message: err.message
-      });
-    }
-  });
+  if (user) {
+    user.sessionToken = undefined;
+
+    await user.save(err => {
+      if (err) {
+        return res.status(500).json({
+          type: "failure",
+          message: err.message
+        });
+      }
+    });
+  }
 
   return res.status(200).json({
-    type: 'failure',
+    type: "success",
     message: `${userId} logged out successfully`
   });
-}
+};
 
 exports.confirmationPost = async (req, res) => {
-
   // Find a matching token
   await Token.findOne({ token: req.params.token_id }, (err, token) => {
-    if (!token) return res.status(400).send({ type: 'not-verified', msg: 'We were unable to find a valid token. Your token my have expired.' });
+    if (!token)
+      return res.status(400).send({
+        type: "not-verified",
+        msg: "We were unable to find a valid token. Your token my have expired."
+      });
 
     // If we found a token, find a matching user
     User.findOne({ _id: token._userId }, async (err, user) => {
-        if (!user) return res.status(400).send({ msg: 'We were unable to find a user for this token.' });
-        if (user.isVerified) return res.status(400).send({ type: 'already-verified', msg: 'This user has already been verified.' });
-
-        // Verify and save the user
-        user.isVerified = true;
-        await user.save(err => {
-          if (err) { return res.status(500).send({ msg: err.message }); }
-          res.status(200).send("The account has been verified! Please log in.");
+      if (!user)
+        return res
+          .status(400)
+          .send({ msg: "We were unable to find a user for this token." });
+      if (user.isVerified)
+        return res.status(400).send({
+          type: "already-verified",
+          msg: "This user has already been verified."
         });
+
+      // Verify and save the user
+      user.isVerified = true;
+      await user.save(err => {
+        if (err) {
+          return res.status(500).send({ msg: err.message });
+        }
+        res.status(200).send("The account has been verified! Please log in.");
+      });
     });
   });
 };
 
 exports.resendTokenPost = (req, res) => {
   User.findOne({ email: req.body.email }, (err, user) => {
-      if (!user) return res.status(400).send({
-        type: 'failure',
-        message: 'We were unable to find a user with that email.'
+    if (!user)
+      return res.status(400).send({
+        type: "failure",
+        message: "We were unable to find a user with that email."
       });
-      if (user.isVerified) return res.status(400).send({
-        type: 'success',
-        message: 'This account has already been verified. Please log in.'
+    if (user.isVerified)
+      return res.status(400).send({
+        type: "success",
+        message: "This account has already been verified. Please log in."
       });
 
-      // Create a verification token, save it, and send email
-      var token = new Token({ _userId: user._id, token: crypto.randomBytes(16).toString('hex') });
+    // Create a verification token, save it, and send email
+    var token = new Token({
+      _userId: user._id,
+      token: crypto.randomBytes(16).toString("hex")
+    });
 
-      // Save the token
-      token.save(err => {
-        if (err) {
-          return res.status(500).send({
-            type: 'failure',
-            message: 'Token cannot be saved',
-            errorDetails: err
-          });
-        }
-        sendEmail({ req, res, token, user });
-      });
+    // Save the token
+    token.save(err => {
+      if (err) {
+        return res.status(500).send({
+          type: "failure",
+          message: "Token cannot be saved",
+          errorDetails: err
+        });
+      }
+      sendEmail({ req, res, token, user });
+    });
   });
 };
 
@@ -243,28 +275,28 @@ exports.getAvatarByUserName = async (req, res) => {
     if (err) {
       return res.status(500).json({
         isValidUser: false,
-        avatar: '',
-        type: 'failure',
-        message: 'Something terrible happened. Try again.'
+        avatar: "",
+        type: "failure",
+        message: "Something terrible happened. Try again."
       });
     }
 
     if (!user) {
       return res.status(401).json({
         isValidUser: false,
-        avatar: ''
+        avatar: ""
       });
     }
 
     if (!user.avatar || !user.avatar.path) {
       return res.status(200).json({
         isValidUser: true,
-        avatar: ''
+        avatar: ""
       });
     } else {
       return res.status(200).json({
         isValidUser: true,
-        avatar: user.avatar.path ? user.avatar.path : ''
+        avatar: user.avatar.path ? user.avatar.path : ""
       });
     }
   });
@@ -276,8 +308,9 @@ exports.getAvatar = async (req, res) => {
   await User.findOne({ _id: userId }, (err, user) => {
     if (err) {
       return res.status(500).json({
-        type: 'failure',
-        message: 'We know you have an avatar but could not retrieve it. Please try again.'
+        type: "failure",
+        message:
+          "We know you have an avatar but could not retrieve it. Please try again."
       });
     }
 
@@ -290,19 +323,19 @@ exports.getAvatar = async (req, res) => {
 
     if (!user.avatar || !user.avatar.path) {
       return res.status(200).json({
-        type: 'success',
+        type: "success",
         avatar: null
       });
     } else {
       return res.status(200).json({
-        type: 'success',
+        type: "success",
         avatar: user.avatar.path
       });
     }
   });
 };
 
-exports.hasUserVoted = async(req, res) => {
+exports.hasUserVoted = async (req, res) => {
   const { fightId, userId } = req.params;
   let match = false;
   let voteObj;
@@ -310,8 +343,8 @@ exports.hasUserVoted = async(req, res) => {
   const user = await User.findById(userId, err => {
     if (err) {
       return res.status(500).json({
-        type: 'failure',
-        message: 'The user is invalid'
+        type: "failure",
+        message: "The user is invalid"
       });
     }
   });
@@ -322,12 +355,12 @@ exports.hasUserVoted = async(req, res) => {
       voteObj = {
         fightId: item.fightId,
         side: item.side
-      }
+      };
     }
   });
 
   res.status(200).json({
-    type: 'success',
+    type: "success",
     match,
     vote: voteObj
   });
@@ -337,11 +370,12 @@ exports.setAvatar = async (req, res) => {
   const { userId } = req.params;
 
   const addImage = async () => {
-
     await User.findOne({ _id: userId }, (err, user) => {
       if (user) {
-        if (env === 'development') {
-          if (!user.avatar) { user.avatar = {}; }
+        if (env === "development") {
+          if (!user.avatar) {
+            user.avatar = {};
+          }
           user.avatar.path = getAvatarPath(req.file.path);
         } else {
           user.avatar.path = req.file.location;
@@ -350,13 +384,14 @@ exports.setAvatar = async (req, res) => {
         user.save(err => {
           if (err) {
             return res.status(500).json({
-              type: 'failure',
-              message: 'The bad news is the image didn\'t save. The good news is… um…'
+              type: "failure",
+              message:
+                "The bad news is the image didn't save. The good news is… um…"
             });
           }
           res.status(200).json({
-            type: 'success',
-            message: 'Woo-hoo! Nice new look.'
+            type: "success",
+            message: "Woo-hoo! Nice new look."
           });
         });
       }
@@ -367,38 +402,35 @@ exports.setAvatar = async (req, res) => {
 };
 
 exports.getUser = async (req, res) => {
-  await User.findOne({ username: req.params.userId })
-    .exec((err, user) => {
-      if (err) {
-        res.status(401).json({
-          type: 'failure'
-        });
-      } else if (!user) {
-        res.status(401).json({
-          type: false
-        });
-      } else {
-        res.status(200).json({
-          type: true,
-          user
-        })
-      }
-    });
+  await User.findOne({ username: req.params.userId }).exec((err, user) => {
+    if (err) {
+      res.status(401).json({
+        type: "failure"
+      });
+    } else if (!user) {
+      res.status(401).json({
+        type: false
+      });
+    } else {
+      res.status(200).json({
+        type: true,
+        user
+      });
+    }
+  });
 };
 
 exports.isUser = async (req, res) => {
   let ref = req.params.userReference;
 
-  let findBy = validator.validate(ref)
-                ? { email: ref }
-                : { username: ref };
+  let findBy = validator.validate(ref) ? { email: ref } : { username: ref };
 
   let user = await User.findOne(findBy);
 
   let username = user !== null ? user.username : null;
 
   res.status(200).json({
-    isUser: (user !== null),
+    isUser: user !== null,
     username
   });
 };
@@ -418,18 +450,20 @@ exports.setWatch = async (req, res) => {
   let watches = user.watching;
   let isWatching = false;
 
-  if (watches.includes(fightId)) { // remove it from array
+  if (watches.includes(fightId)) {
+    // remove it from array
     user.watching.splice(watches.indexOf(fightId), 1);
-  } else { // add it to array
-    user.watching.push(fightId)
+  } else {
+    // add it to array
+    user.watching.push(fightId);
     isWatching = true;
   }
 
   user.save(err => {
     if (err) {
       return res.status(500).json({
-        type: 'failure',
-        message: 'Update failed. And it\'s our fault. Sorry about that.'
+        type: "failure",
+        message: "Update failed. And it's our fault. Sorry about that."
       });
     } else {
       return res.status(200).json({
