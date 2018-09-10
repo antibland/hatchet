c37081bbd4e81a98dad008e8a5344f17868d7a5c
Andy Hoffman <antibland@gmail.com>
2018-09-09 23:46:02 -0700

    Trim transporter options

--- controllers/user.js
+++ controllers/user.js
@@ -23,7 +23,6 @@ const sendEmail = opts => {
   let transporter = nodemailer.createTransport({
     host: "smtp.ethereal.email",
     port: 587,
-    secure: false, // true for 465, false for other ports
     auth: {
       user: process.env.EMAIL_USER, // generated ethereal user
       pass: process.env.EMAIL_PASS // generated ethereal password
