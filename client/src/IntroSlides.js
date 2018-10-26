1eff4a2c05b5f4ff3de5ff983ca3c713170ee2c2
Andy Hoffman <antibland@gmail.com>
2018-10-26 03:11:47 -0700

    Spelling. Thanks @ElectricGlitterbug

--- client/src/IntroSlides.js
+++ client/src/IntroSlides.js
@@ -1,38 +1,58 @@
-import React, { Component } from 'react'
-import Swiper from 'react-id-swiper';
+import React, { Component } from "react";
+import Swiper from "react-id-swiper";
 
 export class IntroSlides extends Component {
   render() {
     const params = {
       pagination: {
-        el: '.swiper-pagination',
+        el: ".swiper-pagination",
         clickable: true
       }
     };
     return (
       <Swiper {...params}>
-        <div className='swiper-slide' id='step1'>
-          <img className='image' src='./svg/icon-intro-argument.svg' alt='Opposing chat bubbles'/>
-          <div className='title'>Share Your Story</div>
-          <div className='text'>Two sides to every story. What's yours?</div>
+        <div className="swiper-slide" id="step1">
+          <img
+            className="image"
+            src="./svg/icon-intro-argument.svg"
+            alt="Opposing chat bubbles"
+          />
+          <div className="title">Share Your Story</div>
+          <div className="text">Two sides to every story. What's yours?</div>
         </div>
-        <div className='swiper-slide' id='step2'>
-          <img className='image' src='./svg/icon-intro-clock.svg' alt='Clock'/>
-          <div className='title'>Wait For Response</div>
-          <div className='text'>Your opponent has 24 hours to respond with a counter argument.</div>
+        <div className="swiper-slide" id="step2">
+          <img className="image" src="./svg/icon-intro-clock.svg" alt="Clock" />
+          <div className="title">Wait For Response</div>
+          <div className="text">
+            Your opponent has 24 hours to respond with a counter argument.
+          </div>
         </div>
-        <div className='swiper-slide' id='step3'>
-          <img className='image' src='./svg/icon-intro-voting.svg' alt='Ballot box'/>
-          <div className='title'>Voting Begins</div>
-          <div className='text'>Aloow the fate of your dispute to be decided by a jury of your peers.</div>
+        <div className="swiper-slide" id="step3">
+          <img
+            className="image"
+            src="./svg/icon-intro-voting.svg"
+            alt="Ballot box"
+          />
+          <div className="title">Voting Begins</div>
+          <div className="text">
+            Allow the fate of your dispute to be decided by a jury of your
+            peers.
+          </div>
         </div>
-        <div className='swiper-slide' id='step4'>
-          <img className='image' src='./svg/icon-intro-trophy.svg' alt='Trophy'/>
-          <div className='title'>Bury The Hatchet</div>
-          <div className='text'>Once the winner is chosen, agree to accept the verdict and let go of the argument.</div>
+        <div className="swiper-slide" id="step4">
+          <img
+            className="image"
+            src="./svg/icon-intro-trophy.svg"
+            alt="Trophy"
+          />
+          <div className="title">Bury The Hatchet</div>
+          <div className="text">
+            Once the winner is chosen, agree to accept the verdict and let go of
+            the argument.
+          </div>
         </div>
-    </Swiper>
-    )
+      </Swiper>
+    );
   }
 }
 
