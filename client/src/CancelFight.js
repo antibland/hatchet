cb7803e77747b979055bd3541c8d85d9685a4a8a
Andy Hoffman <antibland@gmail.com>
2018-10-30 15:57:30 -0700

    Replace native cancel modal with custom modal

--- client/src/CancelFight.js
+++ client/src/CancelFight.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { auth } from "./Auth";
 import styled from "styled-components";
+import Modal from "./shared/components/Modal";
 
 const CancelButton = styled.button`
   padding: 1em 2.5em !important;
@@ -15,33 +16,53 @@ class CancelFight extends Component {
   constructor(props) {
     super(props);
 
+    this.state = { isModalOpen: false };
     this.handleClick = this.handleClick.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+    this.mainAction = this.mainAction.bind(this);
   }
 
   handleClick() {
+    this.setState({ isModalOpen: true });
+  }
+
+  handleCancel() {
+    this.setState({ isModalOpen: false });
+  }
+
+  mainAction() {
     const { fightId } = this.props;
 
     // '/api/:userId/:fightId' => fightApi.cancelFight
-    if (
-      window.confirm(
-        "Are you sure? Not to scare you, but deleting a fight cannot be undone."
-      )
-    ) {
-      fetch(`/api/${auth.user.userid}/${fightId}`, {
-        method: "DELETE"
-      })
-        .then(res => res.json())
-        .then(() => {
-          window.location.reload();
-        });
-    }
+    fetch(`/api/${auth.user.userid}/${fightId}`, {
+      method: "DELETE"
+    })
+      .then(res => res.json())
+      .then(() => {
+        window.location.reload();
+      });
   }
 
   render() {
+    const { isModalOpen } = this.state;
+    const { classes } = this.props;
+
     return (
-      <CancelButton className={this.props.classes} onClick={this.handleClick}>
-        Cancel
-      </CancelButton>
+      <>
+        <CancelButton className={classes} onClick={this.handleClick}>
+          Cancel
+        </CancelButton>
+        <Modal
+          isOpen={isModalOpen}
+          onAction={this.mainAction}
+          onCancel={this.handleCancel}
+        >
+          <p>
+            Are you sure? Not to scare you, but deleting a fight cannot be
+            undone.
+          </p>
+        </Modal>
+      </>
     );
   }
 }
